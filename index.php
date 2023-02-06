72341b7334df5094620616716758c0b6d0e85bb3
GrantN <s3954897@student.rmit.edu.au>
2023-02-06 21:16:09 +1000

    Initial commit

---
+++ index.php
@@ -0,0 +1,32 @@
+
+<html>
+    <head>
+        <link href="style.css">
+    </head>
+    <body>
+        <header>
+            <h1>Friendly Grocer - Victoria Ave</h1>
+        </header>
+        <main>
+            <div>
+                <h2>Menu Group 1</h2>
+            </div>
+            <div>
+                <h2>Menu Group 2</h2>
+            </div>
+            <div>
+                <h2>Menu Group 3</h2>
+            </div>
+            <div>
+                <h2>Menu Group 4</h2>
+            </div>
+
+        </main>
+        <footer>
+            <div>
+                <p>Something important</p>
+            </div>
+        </footer>
+
+    </body>
+</html>
