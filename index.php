0cdccb2928d269b3809be2b0e4124fc03245f7d7
GrantN <s3954897@student.rmit.edu.au>
2023-02-10 09:52:09 +1000

    import for fonts

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="refresh" content="30">
         <title>Friendly Grocer - Victoria Ave</title>
         <link id="stylecss" type="text/css" rel="stylesheet" href="style.css?t=<?= filemtime("style.css"); ?>">
     </head>
