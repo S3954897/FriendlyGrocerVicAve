95e61a0db38465f6c762cb9cc782dd905fb535c4
GrantN <s3954897@student.rmit.edu.au>
2023-02-10 08:49:10 +1000

    Modified

--- index.php
+++ index.php
@@ -1,31 +1,208 @@
-
-<html>
+<!DOCTYPE html>
+<html lang="en">
     <head>
-        <link href="style.css">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Friendly Grocer - Victoria Ave</title>
+        <link id="stylecss" type="text/css" rel="stylesheet" href="style.css?t=<?= filemtime("style.css"); ?>">
     </head>
     <body>
         <header>
-            <h1>Friendly Grocer - Victoria Ave</h1>
+            <h1>Breakfast Menu</h1>
         </header>
         <main>
             <div>
-                <h2>Menu Group 1</h2> 
+                <article>
+                    <table>
+                        <tr>
+                            <td class="col1">Hash Brown</td>
+                            <td>$1.00</td>
+                        </tr>
+                        <tr>
+                            <td class="col1">Bacon & Egg Roll with BBQ sauce</td>
+                            <td>$5.00</td>
+                        </tr>
+                        <tr>
+                            <td  class="col1">Bacon & Egg Roll with hollandaise sauce</td>
+                            <td>$5.50</td>
+                        </tr>
+                        <tr>
+                            <td class="col1">Bacon & Egg Roll Muffin</td>
+                            <td>$4.30</td>
+                        </tr>
+                        <tr>
+                            <td class="col1">Sausages & Gravy</td>
+                            <td>$6.50</td>
+                        </tr>
+                    </table>
+                </article>
             </div>
             <div>
-                <h2>Menu Group 2</h2>
+                <h2>Whole Chicken</h2>
+                <article>
+                    <table>
+                        <tr>
+                            <td class="col1">plus 1.25L Coca-Cola Variety</td>
+                            <td class="col2">$14.49</td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    </table>
+                </article>
             </div>
             <div>
-                <h2>Menu Group 3</h2>
+                <h2>Bacon & Egg Muffin</h2>
+                <article>
+                    <table>
+                        <tr>
+                            <td class="col1">plus 600ml 'Mount Franklin</td>
+                            <td>$6.50</td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    </table>
+                </article>
             </div>
             <div>
-                <h2>Menu Group 4</h2>
+                <h2>Half Chicken</h2>
+                <article>
+                    <table>
+                        <tr>
+                            <td class="col1">plus 600ml Coca-Cola Variety</td>
+                            <td class="col2">$8.99</td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    </table>
+                </article>
+            </div>
+            <div>
+                <h2>Breakfast Combo</h2>
+                <article>
+                    <table>
+                        <tr>
+                            <td class="col1">Bacon, Egg & Hashbrown</td>
+                            <td class="col2">$8.99</td>
+                        </tr>
+                        <tr>
+                            <td>plus 600ml 'Mount Franklin'</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    </table>
+                </article>
+            </div>
+            <div>
+                <h2>Half Chicken</h2>
+                <article>
+                    <table>
+                        <tr>
+                            <td class="col1">plus 600ml Coca-Cola Variety</td>
+                            <td class="col2">$8.99</td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    </table>
+                </article>
+            </div>
+            <div>
+                <h2>Breakfast Coffee Combo</h2>
+                <article>
+                    <table>
+                        <tr>
+                            <td class="col1">Bacon, Egg & Hashbrown</td>
+                            <td class="col2">$7.00</td>
+                        </tr>
+                        <tr>
+                            <td>plus regular coffee</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    </table>
+                </article>
+            </div>
+
+            <div>
+                <h2>Fresh Sandwich</h2>
+                <article>
+                    <table>
+                        <tr>
+                            <td class="col1">plus 600ml 'Mount Franklin'</td>
+                            <td class="col2">$6.00</td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    </table>
+                </article>
             </div>
 
         </main>
         <footer>
-            <div>
-                <p>Something important</p>
-            </div>
+
         </footer>
 
     </body>
